185bcab6d973fe869f800f8015fe0020c7a3608b
Suhiteswar <105535366+suhit123@users.noreply.github.com>
2025-01-29 20:25:26 +0530

    changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,16 @@
-import  React from 'react';
+import  React, { useState } from 'react';
 import Nav from "@/components/nav";
 import { Scanner } from '@yudiel/react-qr-scanner';
 
 export default function Home() {
+  const [scanValue, setScanValue] =useState(null);
   return (
    <>
     <Nav/>
+    <div className='flex justify-center items-center h-screen max-w-96'>
+      
     <Scanner onScan={(result) => console.log(result)} />
+    </div>
    </>
   );
 }
