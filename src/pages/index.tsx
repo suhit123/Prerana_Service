52bad4835b498579e7119932cc780a11adc267ca
Suhiteswar <105535366+suhit123@users.noreply.github.com>
2025-01-29 20:38:54 +0530

    changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,20 +3,23 @@ import Nav from "@/components/nav";
 import { Scanner } from "@yudiel/react-qr-scanner";
 
 export default function Home() {
-  const [scanValue, setScanValue]:any =useState(null);
+  const [scanValue, setScanValue] = useState<string | null>(null); // Specify type here
+
   return (
     <>
       <Nav />
       <div className="max-w-96">
-        <Scanner onScan={(result) => {
-          if(result.length > 0) {
-            console.log(result[0].rawValue);
-            setScanValue(result[0].rawValue);
-          }
-        }}/>
+        <Scanner
+          onScan={(result) => {
+            if (result.length > 0) {
+              console.log(result[0].rawValue);
+              setScanValue(result[0].rawValue);
+            }
+          }}
+        />
         <div className="flex justify-center mt-5">
           <p className="text-lg">Scanned Value: {scanValue}</p>
-          </div>
+        </div>
       </div>
     </>
   );
