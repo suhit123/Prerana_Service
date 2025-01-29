8c40b7a0655c9e5bf38fcfab14609258e8a30276
Suhiteswar <105535366+suhit123@users.noreply.github.com>
2025-01-29 20:26:08 +0530

    changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import Nav from "@/components/nav";
 import { Scanner } from '@yudiel/react-qr-scanner';
 
 export default function Home() {
-  const [scanValue, setScanValue] =useState(null);
+  // const [scanValue, setScanValue] =useState(null);
   return (
    <>
     <Nav/>
