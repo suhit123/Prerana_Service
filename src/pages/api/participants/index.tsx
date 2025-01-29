2113f29ee2339494a2dd25f331fa1afdb4d0e3d1
Suhiteswar <105535366+suhit123@users.noreply.github.com>
2025-01-29 20:01:25 +0530

    changes

--- src/pages/api/participants/index.tsx
+++ src/pages/api/participants/index.tsx
@@ -1,19 +1,24 @@
-import dbConnect from '@/utils/dbConnect'
-import PassesSchema from '@/models/Passes'
+import { NextApiRequest, NextApiResponse } from 'next';
+import dbConnect from '@/utils/dbConnect';
+import PassesSchema from '@/models/Passes';
+
 dbConnect();
-export default async (req:any,res:any)=>{
-    const {method}=req;
-    switch(method){
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+    const { method } = req;
+    switch (method) {
         case 'GET':
-            try{
-                const participants=await PassesSchema.find({});
-                res.status(200).json({success:true,data:participants});
-            }catch(err:any){
+            try {
+                const participants = await PassesSchema.find({});
+                res.status(200).json({ success: true, data: participants });
+            } catch (err) {
                 console.error(err);
-                res.status(400).json({success:false});
+                res.status(400).json({ success: false });
             }
             break;
         default:
-            res.status(400).json({success:false});
-    };
+            res.status(400).json({ success: false });
+    }
 };
+
+export default handler;
